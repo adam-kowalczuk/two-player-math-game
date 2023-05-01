7873a0f7123b8ac5841a54dac9b5417eaf7851a1
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-05-01 15:57:03 -0700

    Create game_over? method to return true if no lives left, add comments to class

--- player.rb
+++ player.rb
@@ -1,14 +1,22 @@
 class Player 
+
+  # Set player name and number of lives
   def initialize(name)
     @name = name
     @lives = 3
   end
 
+  # Decrease player lives by 1 
   def lose_life
     @lives -= 1
+  end
+
+  # Check if number of lives equals 0
+  def game_over?
+    @lives == 0
   end
 end
 
 jeff = Player.new('Jeff')
 jeff.lose_life
-p jeff
+puts jeff.game_over?
