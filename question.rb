6aaf9739d11ff813b904ed55d8708ff631b8499b
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-05-01 16:06:28 -0700

    Rename num1 method to generate_numbers, add instance variable to create another number within method

--- question.rb
+++ question.rb
@@ -1,9 +1,11 @@
 class Question 
-  def num1
+  def generate_numbers
     @num1 = rand(1..20)
+    @num2 = rand(1..20)
   end
+
 end
 
 question = Question.new
-question.num1
+question.generate_numbers
 p question
