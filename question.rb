7fc8937ff256ce310ad093e437662eac623b5076
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-05-01 16:44:49 -0700

    Add correct_answer? method which checks whether the supplied answer matches the sum of num1 and num2

--- question.rb
+++ question.rb
@@ -1,11 +1,15 @@
 class Question 
-  def generate_numbers
+  # Will need to read num1 and num2 outside of class
+  attr_reader :num1, :num2
+
+  # Generate two random numbers between 1 and 20
+  def initialize
     @num1 = rand(1..20)
     @num2 = rand(1..20)
   end
 
+  # Check whether answer supplied matches sum of both numbers
+  def correct_answer?(answer)
+    answer == @num1 + @num2
+  end
 end
-
-question = Question.new
-question.generate_numbers
-p question
