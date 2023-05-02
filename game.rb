0888497f6a1cad19fbfde07bae837e56aefbdccc
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-05-01 18:17:54 -0700

    Add definition for turn method

--- game.rb
+++ game.rb
@@ -1,13 +1,21 @@
 require './player.rb'
 require './question.rb'
 
-class Game 
+@player1 = Player.new('Player 1')
+@player2 = Player.new('Player 2')
 
-  def initialize
-    @player1 = Player.new('Player 1')
-    @player2 = Player.new('Player 2')
+def turn (player)
+  question = Question.new
+
+  puts "#{player.name}: what does #{question.num1} plus #{question.num2} equal?"
+
+  answer = $stdin.gets.chomp
+
+  if question.correct_answer?(answer)
+    puts "Correct!"
+  else 
+    puts "Wrong answer! You lose a life!"
+    player.lose_life
   end
-
-  def turn (player)
-    question = Question.new
+end
 
