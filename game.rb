0fe860cc512512e9377638acc49b9f5d9f96ce54
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-05-02 09:35:54 -0700

    Refactor play method to make it DRY

--- game.rb
+++ game.rb
@@ -1,50 +1,56 @@
 class Game
-  def play
+  def initialize
     # Create two new players
-    player1 = Player.new('Player 1')
-    player2 = Player.new('Player 2')
+    @player1 = Player.new('Player 1')
+    @player2 = Player.new('Player 2')
 
-    # Run player through an entire turn
-    def turn (player)
-      # Create a new question
-      question = Question.new
+    @players = [@player1, @player2]
+  end
 
-      # Prompt player with new question
-      puts "#{player.name}: what does #{question.num1} plus #{question.num2} equal?"
+  # Run player through an entire turn
+  def turn (player)
+    # Create a new question
+    question = Question.new
 
-      # Store answer via CL input
-      answer = $stdin.gets.chomp
+    # Prompt player with new question
+    puts "#{player.name}: what does #{question.num1} plus #{question.num2} equal?"
 
-      # Check if player's answer is correct
-      if question.correct_answer?(answer)
-        puts "#{player.name}: YES! You are correct!"
-      else 
-        puts "#{player.name}: Seriously? No!"
-        # If incorrect, decrease player lives by 1
-        player.lose_life
-      end
+    # Store answer via CL input
+    answer = $stdin.gets.chomp
+
+    # Check if player's answer is correct
+    if question.correct_answer?(answer)
+      puts "#{player.name}: YES! You are correct!"
+    else 
+      puts "#{player.name}: Seriously? No!"
+      # If incorrect, decrease player lives by 1
+      player.lose_life
     end
+  end
 
+  def play
     # Run loop until one of the players loses
     loop do
       # Player 1 goes first
-      turn(player1)
+      turn(@players[0])
       # After their turn, provide a lives count for each player
-      puts "P1: #{player1.lives}/3 vs P2: #{player2.lives}/3"
+      puts "P1: #{@player1.lives}/3 vs P2: #{@player2.lives}/3"
       # Break out of the loop if Player 1 has no remaining lives
-      break puts "Player 2 wins with a score of #{player2.lives}/3" if player1.game_over?
-
+      break puts "#{@players[1].name} wins with a score of #{@players[1].lives}/3" if @players[0].game_over?
+      
       puts "---NEW TURN---"
+      
+      @players.reverse!
 
-      # Now it's Player 2's turn 
-      turn(player2)
-      # After their turn, provide a lives count for each player
-      puts "P1: #{player1.lives}/3 vs P2: #{player2.lives}/3"
-      # Break out of the loop if Player 2 has no remaining lives
-      break puts "Player 1 wins with a score of #{player1.lives}/3" if player2.game_over?
+      # # Now it's Player 2's turn 
+      # turn(@player2)
+      # # After their turn, provide a lives count for each player
+      # puts "P1: #{@player1.lives}/3 vs P2: #{@player2.lives}/3"
+      # # Break out of the loop if Player 2 has no remaining lives
+      # break puts "Player 1 wins with a score of #{@player1.lives}/3" if @player2.game_over?
 
-      # If neither player had won, start a new round
-      puts "---NEW TURN---"
+      # # If neither player had won, start a new round
+      # puts "---NEW TURN---"
     end
 
     # Once there is a victor, display below messages and end game
